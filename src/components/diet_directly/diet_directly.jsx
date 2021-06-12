aa9054186f4a61c5dc1bdd3e88797aa704fc9e7d
zzmin13 <zzzz1997@naver.com>
2021-06-12 16:16:19 +0900

    식사 직접 추가 디자인

--- src/components/diet_directly/diet_directly.jsx
+++ src/components/diet_directly/diet_directly.jsx
@@ -4,23 +4,39 @@ const DietDirectly = ({ database, loadUserInformation, uid, user }) => {
   // 식사 직접 추가하는 컴포넌트
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>직접 추가하기</h1>;
-      <form>
-        <div>
-          <label htmlFor="name">음식명</label>
-          <input type="text" id="name" />
+      <h1 className={styles.title}>직접 추가하기</h1>
+      <form className={styles.form}>
+        <div className={styles.item}>
+          <label className={styles.item_name} htmlFor="name">
+            음식명
+          </label>
+          <div className={styles.item_second}>
+            <input className={styles.item_input} type="text" id="name" />
+          </div>
         </div>
-        <div>
-          <label htmlFor="amount">음식량</label>
-          <input type="text" id="amount" />
-          <label htmlFor="amount">g</label>
+        <div className={styles.item}>
+          <label className={styles.item_name} htmlFor="amount">
+            음식량
+          </label>
+          <div className={styles.item_second}>
+            <input className={styles.item_input} type="text" id="amount" />
+            <label className={styles.item_behind} htmlFor="amount">
+              g
+            </label>
+          </div>
         </div>
-        <div>
-          <label htmlFor="kcal">칼로리</label>
-          <input type="text" id="kcal" />
-          <label htmlFor="kcal">kcal</label>
+        <div className={styles.item}>
+          <label className={styles.item_name} htmlFor="kcal">
+            칼로리
+          </label>
+          <div className={styles.item_second}>
+            <input className={styles.item_input} type="text" id="kcal" />
+            <label className={styles.item_behind} htmlFor="kcal">
+              kcal
+            </label>
+          </div>
         </div>
-        <button>추가하기</button>
+        <button className={styles.addButton}>추가하기</button>
       </form>
     </div>
   );

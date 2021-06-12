c9ff0a4c3961199d17a9e44a25aa5a09ca260c0e
zzmin13 <zzzz1997@naver.com>
2021-06-12 15:37:19 +0900

    diet 페이지 개선

--- src/routes/diet/diet.jsx
+++ src/routes/diet/diet.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect } from "react";
 import styles from "./diet.module.css";
+import DietItem from "../../components/diet_item/diet_item";
 const Diet = ({ authService, foodSearch, user, database, history }) => {
+  const currentYear = `${new Date().getFullYear()}`;
+  const currentMonth =
+    new Date().getMonth() + 1 < 10
+      ? `0${new Date().getMonth() + 1}`
+      : `${new Date().getMonth() + 1}`;
+  const currentDate =
+    new Date().getDate() < 10
+      ? `0${new Date().getDate()}`
+      : `${new Date().getDate()}`;
+  const current = currentYear + currentMonth + currentDate;
+
   const goDietAddPage = () => {
     history.push("/diet/add");
   };
@@ -20,41 +32,178 @@ const Diet = ({ authService, foodSearch, user, database, history }) => {
               <i className={`fas fa-utensils ${styles.icon}`}></i>
               <span>오늘의 식사</span>
             </div>
-            <div className={styles.diet}>
-              <div className={styles.diet_item}>
-                <div className={styles.diet_item_title}>
-                  <h1>아침</h1>
+            <div className={`${styles.text} ${styles.text_column}`}>
+              <div className={styles.text_column_title}>
+                <div>
+                  <span className={styles.bold}>칼로리 합계</span>
                 </div>
-                <ul>
-                  <li className={styles.food_li}>
-                    토마토(데친것) 1회분 (17kcal)
-                  </li>
-                  <li className={styles.food_li}>
-                    닭가슴살 스테이크(오리지널) 1인분(팩) (199kcal)
-                  </li>
-                </ul>
+                <div className={styles.text_text2}>
+                  <span>
+                    {user.userDiary[current].diet.totalCalories
+                      ? user.userDiary[current].diet.totalCalories
+                      : 0}{" "}
+                  </span>
+                  <span> Kcal</span>
+                </div>
               </div>
-              <div className={styles.diet_item}>
-                <h1>점심</h1>
-                <ul>
-                  <li className={styles.food_li}>
-                    토마토(데친것) 1회분 (17kcal)
-                  </li>
-                  <li className={styles.food_li}>
-                    닭가슴살 스테이크(오리지널) 1인분(팩) (199kcal)
-                  </li>
-                </ul>
-              </div>
-              <div className={styles.diet_item}>
-                <h1>저녁</h1>
-                <ul>
-                  <li className={styles.food_li}>
-                    토마토(데친것) 1회분 (17kcal)
-                  </li>
-                  <li className={styles.food_li}>
-                    닭가슴살 스테이크(오리지널) 1인분(팩) (199kcal)
-                  </li>
-                </ul>
+              <div className={styles.text_column_child}>
+                <div className={styles.text_diet}>
+                  <div className={styles.text_diet_meta}>
+                    <div className={styles.text_diet_time}>
+                      <h1 className={styles.text_diet_title}>아침</h1>
+                      <div className={styles.text_text2}>
+                        <span>
+                          {user.userDiary[current].diet.breakfast.totalCalories
+                            ? user.userDiary[current].diet.breakfast
+                                .totalCalories
+                            : 0}
+                        </span>
+                        <span>Kcal</span>
+                      </div>
+                    </div>
+                    <ul className={styles.text_diet_list}>
+                      {user.userDiary[current].diet.breakfast
+                        ? Object.keys(
+                            user.userDiary[current].diet.breakfast
+                          ).map((element) => {
+                            if (element !== "totalCalories") {
+                              return (
+                                <DietItem
+                                  key={
+                                    user.userDiary[current].diet.breakfast[
+                                      element
+                                    ].id
+                                  }
+                                  diet={
+                                    user.userDiary[current].diet.breakfast[
+                                      element
+                                    ]
+                                  }
+                                />
+                              );
+                            }
+                          })
+                        : null}
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.text_diet}>
+                  <div className={styles.text_diet_meta}>
+                    <div className={styles.text_diet_time}>
+                      <h1 className={styles.text_diet_title}>점심</h1>
+                      <div className={styles.text_text2}>
+                        <span>
+                          {user.userDiary[current].diet.lunch.totalCalories
+                            ? user.userDiary[current].diet.lunch.totalCalories
+                            : 0}
+                        </span>
+                        <span>Kcal</span>
+                      </div>
+                    </div>
+                    <ul className={styles.text_diet_list}>
+                      {user.userDiary[current].diet.lunch
+                        ? Object.keys(user.userDiary[current].diet.lunch).map(
+                            (element) => {
+                              if (element !== "totalCalories") {
+                                return (
+                                  <DietItem
+                                    key={
+                                      user.userDiary[current].diet.lunch[
+                                        element
+                                      ].id
+                                    }
+                                    diet={
+                                      user.userDiary[current].diet.lunch[
+                                        element
+                                      ]
+                                    }
+                                  />
+                                );
+                              }
+                            }
+                          )
+                        : null}
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.text_diet}>
+                  <div className={styles.text_diet_meta}>
+                    <div className={styles.text_diet_time}>
+                      <h1 className={styles.text_diet_title}>저녁</h1>
+                      <div className={styles.text_text2}>
+                        <span>
+                          {user.userDiary[current].diet.dinner.totalCalories
+                            ? user.userDiary[current].diet.dinner.totalCalories
+                            : 0}
+                        </span>
+                        <span>Kcal</span>
+                      </div>
+                    </div>
+                    <ul className={styles.text_diet_list}>
+                      {user.userDiary[current].diet.dinner
+                        ? Object.keys(user.userDiary[current].diet.dinner).map(
+                            (element) => {
+                              if (element !== "totalCalories") {
+                                return (
+                                  <DietItem
+                                    key={
+                                      user.userDiary[current].diet.dinner[
+                                        element
+                                      ].id
+                                    }
+                                    diet={
+                                      user.userDiary[current].diet.dinner[
+                                        element
+                                      ]
+                                    }
+                                  />
+                                );
+                              }
+                            }
+                          )
+                        : null}
+                    </ul>
+                  </div>
+                </div>
+                <div className={styles.text_diet}>
+                  <div className={styles.text_diet_meta}>
+                    <div className={styles.text_diet_time}>
+                      <h1 className={styles.text_diet_title}>간식</h1>
+                      <div className={styles.text_text2}>
+                        <span>
+                          {user.userDiary[current].diet.dessert.totalCalories
+                            ? user.userDiary[current].diet.dessert.totalCalories
+                            : 0}
+                        </span>
+                        <span>Kcal</span>
+                      </div>
+                    </div>
+                    <ul className={styles.text_diet_list}>
+                      {user.userDiary[current].diet.dessert
+                        ? Object.keys(user.userDiary[current].diet.dessert).map(
+                            (element) => {
+                              if (element !== "totalCalories") {
+                                return (
+                                  <DietItem
+                                    key={
+                                      user.userDiary[current].diet.dessert[
+                                        element
+                                      ].id
+                                    }
+                                    diet={
+                                      user.userDiary[current].diet.dessert[
+                                        element
+                                      ]
+                                    }
+                                  />
+                                );
+                              }
+                            }
+                          )
+                        : null}
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
             <button onClick={goDietAddPage} className={styles.button}>

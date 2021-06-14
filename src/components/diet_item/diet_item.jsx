5a708ef4bfb650fe057fced6ac0b866faf4de6c5
zzmin13 <zzzz1997@naver.com>
2021-06-14 15:42:38 +0900

    식사 삭제 버튼 UI

--- src/components/diet_item/diet_item.jsx
+++ src/components/diet_item/diet_item.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import styles from "./diet_item.module.css";
 
 const DietItem = (props) => {
   const {
     diet: { kcal, name, totalSize },
   } = props;
-  return <li>{`${name} (${totalSize}g, ${kcal}kcal)`}</li>;
+  const handleDietDelete = () => {};
+  return (
+    <div className={styles.container}>
+      <li className={styles.diet}>
+        <span>{`${name} (${totalSize}g, ${kcal}kcal)`}</span>
+      </li>
+      <button onClick={handleDietDelete} className={styles.delete_button}>
+        ×
+      </button>
+    </div>
+  );
 };
 
 export default DietItem;

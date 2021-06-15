6fd7d413d9c47a7fa8a181a96700ea3eddac3803
zzmin13 <zzzz1997@naver.com>
2021-06-15 13:20:58 +0900

    물 추가하기 페이지 UI 완성

--- src/routes/water_add/water_add.jsx
+++ src/routes/water_add/water_add.jsx
@@ -5,6 +5,7 @@ import styles from "./water_add.module.css";
 const WaterAdd = ({ authService, history }) => {
   const mlRef = useRef();
   const cupRef = useRef();
+  const timeRef = useRef();
 
   useEffect(() => {
     authService.onAuthStateChanged((USER) => {
@@ -16,18 +17,22 @@ const WaterAdd = ({ authService, history }) => {
   const onIncrease = (event) => {
     if (event.currentTarget.id === "cup") {
       cupRef.current.value = Number(cupRef.current.value) + 1;
+      mlRef.current.value = cupRef.current.value * 170;
     } else if (event.currentTarget.id === "ml") {
       mlRef.current.value = Number(mlRef.current.value) + 170;
+      cupRef.current.value = mlRef.current.value / 170;
     }
   };
   const onDecrease = (event) => {
     if (event.currentTarget.id === "cup") {
-      if (Number(cupRef.current.value) !== 0) {
+      if (Number(cupRef.current.value) - 1 >= 0) {
         cupRef.current.value = Number(cupRef.current.value) - 1;
+        mlRef.current.value = cupRef.current.value * 170;
       }
     } else if (event.currentTarget.id === "ml") {
-      if (Number(mlRef.current.value) !== 170) {
+      if (Number(mlRef.current.value) - 170 >= 0) {
         mlRef.current.value = Number(mlRef.current.value) - 170;
+        cupRef.current.value = mlRef.current.value / 170;
       }
     }
   };
@@ -44,76 +49,96 @@ const WaterAdd = ({ authService, history }) => {
         <div className={styles.title}>
           <span>물 추가하기</span>
         </div>
-        <div className={styles.forms}>
-          <form className={styles.form}>
-            <button
-              onClick={onIncrease}
-              type="button"
-              className={styles.updown_button}
-              id="ml"
-            >
-              <i
-                className={`fas fa-chevron-up ${styles.icon_up} ${styles.icon}`}
-              ></i>
-            </button>
-            <div className={styles.cup_box}>
-              <input
-                id="ml"
-                onChange={onChange}
-                ref={mlRef}
-                className={styles.input}
-                type="number"
-                defaultValue={170}
-                step={170}
-              />
-              <span className={styles.ml_text}>ml</span>
-            </div>
-            <button
-              onClick={onDecrease}
-              type="button"
-              className={styles.updown_button}
-              id="ml"
-            >
-              <i
-                className={`fas fa-chevron-down ${styles.icon_down} ${styles.icon}`}
-              ></i>
-            </button>
-          </form>
-          <form className={styles.form}>
-            <button
-              onClick={onIncrease}
-              type="button"
-              className={styles.updown_button}
-              id="cup"
-            >
-              <i
-                className={`fas fa-chevron-up ${styles.icon_up} ${styles.icon}`}
-              ></i>
-            </button>
-            <div className={styles.cup_box}>
-              <input
-                id="cup"
-                onChange={onChange}
-                ref={cupRef}
-                className={styles.input}
-                type="number"
-                defaultValue={1.0}
-                step={1.0}
-              />
-              <span className={styles.cup_text}>종이컵</span>
-            </div>
-            <button
-              onClick={onDecrease}
-              type="button"
-              className={styles.updown_button}
-              id="cup"
-            >
-              <i
-                className={`fas fa-chevron-down ${styles.icon_down} ${styles.icon}`}
-              ></i>
-            </button>
-          </form>
+        <div className={styles.helper}>
+          <h1 className={styles.helper_text}>
+            💡 숫자를 누르면 물의 양을 직접 입력할 수 있어요.
+          </h1>
         </div>
+        <div className={styles.forms}>
+          <div className={styles.item}>
+            <select
+              ref={timeRef}
+              className={`${styles.time_select} ${styles.item}`}
+            >
+              <option>아침</option>
+              <option>점심</option>
+              <option>저녁</option>
+            </select>
+          </div>
+          <div className={styles.item}>
+            <form className={styles.form}>
+              <button
+                onClick={onIncrease}
+                type="button"
+                className={styles.updown_button}
+                id="ml"
+              >
+                <i
+                  className={`fas fa-chevron-up ${styles.icon_up} ${styles.icon}`}
+                ></i>
+              </button>
+              <div className={styles.cup_box}>
+                <input
+                  id="ml"
+                  onChange={onChange}
+                  ref={mlRef}
+                  className={styles.input}
+                  type="number"
+                  defaultValue={170}
+                  step={170}
+                />
+                <span className={styles.ml_text}>ml</span>
+              </div>
+              <button
+                onClick={onDecrease}
+                type="button"
+                className={styles.updown_button}
+                id="ml"
+              >
+                <i
+                  className={`fas fa-chevron-down ${styles.icon_down} ${styles.icon}`}
+                ></i>
+              </button>
+            </form>
+          </div>
+          <div className={styles.item}>
+            <form className={styles.form}>
+              <button
+                onClick={onIncrease}
+                type="button"
+                className={styles.updown_button}
+                id="cup"
+              >
+                <i
+                  className={`fas fa-chevron-up ${styles.icon_up} ${styles.icon}`}
+                ></i>
+              </button>
+              <div className={styles.cup_box}>
+                <input
+                  id="cup"
+                  onChange={onChange}
+                  ref={cupRef}
+                  className={styles.input}
+                  type="number"
+                  defaultValue={1.0}
+                  step={1.0}
+                />
+                <span className={styles.cup_text}>종이컵</span>
+              </div>
+              <button
+                onClick={onDecrease}
+                type="button"
+                className={styles.updown_button}
+                id="cup"
+              >
+                <i
+                  className={`fas fa-chevron-down ${styles.icon_down} ${styles.icon}`}
+                ></i>
+              </button>
+            </form>
+          </div>
+        </div>
+        <button className={styles.button}>추가하기</button>
       </div>
     </div>
   );

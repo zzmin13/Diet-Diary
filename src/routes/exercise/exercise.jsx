d82aaf46f16b89129968b1180a98bdc696536fda
zzmin13 <zzzz1997@naver.com>
2021-06-16 16:02:28 +0900

    exercise 페이지 기본 틀 구현

--- src/routes/exercise/exercise.jsx
+++ src/routes/exercise/exercise.jsx
@@ -1,13 +1,51 @@
 import React from "react";
+import styles from "./exercise.module.css";
+const Exercise = ({ database, history, user }) => {
+  const currentYear = `${new Date().getFullYear()}`;
+  const currentMonth =
+    new Date().getMonth() + 1 < 10
+      ? `0${new Date().getMonth() + 1}`
+      : `${new Date().getMonth() + 1}`;
+  const currentDate =
+    new Date().getDate() < 10
+      ? `0${new Date().getDate()}`
+      : `${new Date().getDate()}`;
+  const current = currentYear + currentMonth + currentDate;
 
-const Exercise = ({ history }) => {
   const goExerciseAddPage = () => {
     history.push("/exercise/add");
   };
   return (
     <>
-      <h1>Exercise</h1>
-      <button onClick={goExerciseAddPage}>운동 입력하기</button>
+      {user.userDiary ? (
+        <div className={styles.container}>
+          <div className={styles.main}>
+            <div className={styles.title}>
+              <i className={`fas fa-dumbbell ${styles.icon}`}></i>
+              <span>오늘의 운동</span>
+            </div>
+            <div className={styles.text_column}>
+              <div className={styles.text_column_title}>
+                <div>
+                  <span className={styles.bold}>칼로리 합계</span>
+                </div>
+                <div className={styles.text_column_title_number}>
+                  <span>
+                    {user.userDiary[current].exercise.totalCalories
+                      ? user.userDiary[current].exercise.totalCalories
+                      : 0}{" "}
+                  </span>
+                  <span> Kcal</span>
+                </div>
+              </div>
+              <div className={styles.text_column_child}></div>
+            </div>
+            <button onClick={goExerciseAddPage} className={styles.button}>
+              운동 추가하기
+            </button>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 };

832923e88060f1de5f626f4f638ff7621552d647
zzmin13 <zzzz1997@naver.com>
2021-07-06 10:30:32 +0900

    비밀번호 재설정 페이지 UI 구현

--- src/routes/find/find.jsx
+++ src/routes/find/find.jsx
@@ -4,7 +4,24 @@ import styles from "./find.module.css";
 const Find = (props) => {
   return (
     <div className={styles.container}>
-      <h1>비밀번호를 잊어버리셨나요?</h1>
+      <h1 className={styles.title}>비밀번호를 잊어버리셨나요?</h1>
+      <form className={styles.form}>
+        <div className={styles.text}>
+          <p className={styles.text_element}>
+            다다에 가입했던 이메일을 입력해주세요.
+          </p>
+          <p className={styles.text_element}>
+            비밀번호 재설정 이메일을 보내드립니다.
+          </p>
+        </div>
+        <input
+          type="email"
+          required={true}
+          className={styles.input}
+          placeholder="이메일"
+        />
+        <button className={styles.button_submit}>재설정 메일 보내기</button>
+      </form>
     </div>
   );
 };
